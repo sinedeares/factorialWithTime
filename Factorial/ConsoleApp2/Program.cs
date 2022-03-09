b962d24fe2da01d41d0f3fc0c3864ceee02559f8
sinedeares <62802443+sinedeares@users.noreply.github.com>
2022-03-09 17:43:58 +0400

    Add files via upload

---
+++ Factorial/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var n = 10000;
+
+            var factorial = new BigInteger(1);
+            for (int i = 1; i <= n; i++)
+                factorial *= i;
+            Console.WriteLine("Факториал числа " + n + " = " + factorial);
+
+            stopwatch.Stop();
+            Console.WriteLine("Время работы программы в миллисекундах: " + stopwatch.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
